8b7f810781cd3bf1139b2853c5f507401f8fe676
Ayusman Avisek Nanda <95304223+IC7400A@users.noreply.github.com>
2025-01-10 12:38:03 +0530

    Update option.c

--- option.c
+++ option.c
@@ -2,313 +2,180 @@
 #include <stdbool.h>
 #include <stdlib.h>
 
-float scan(){
+// Function to scan float values
+float scan() {
     float num;
     scanf("%f", &num);
     return num;
 }
 
-char scanc(){
+// Function to scan char values
+char scanc() {
     char choice;
-    printf("Do you want to continue ? \n");
+    printf("Do you want to continue?\n");
     printf("---- ---- ---- ---- \n");
     printf("y = Yes \n");
     printf("n = Exit \n");
     printf("m = Return to Menu \n");
     printf("---- ---- ---- ---- \n");
-    scanf(" %c", &choice);
+    scanf(" %c", &choice); // Note the space before %c to consume any trailing whitespace
     return choice;
 }
 
-char err(){
-  printf("Wrong Input :()\n Try again :) \n");
+// Error handling function for invalid inputs
+void err() {
+    printf("Wrong Input :()\nTry again :) \n");
 }
 
-float plus(){
-    while(true){
-
-        float a, b, c;
-        printf("Enter Number you want to add \n");
-        a = scan();
-        printf("+ \n");
-        b = scan();
-        c = a + b;
-        printf("Answer of ans is %g \n", c);
-        return c;
-
-        
-      }
-
+// Function to perform addition
+float add() {
+    float a, b;
+    printf("Enter Number you want to add: \n");
+    a = scan();
+    printf("+ \n");
+    b = scan();
+    float result = a + b;
+    printf("Answer: %g \n", result);
+    return result;
 }
 
-float minus(){
-    while(true){
-
-        float a, b, c;
-        printf("Enter Number you want to minus from \n");
-        a = scan();
-        printf("- \n");
-        b = scan();
-        c = a - b;
-        printf("Answer of ans is %g \n", c);
-        return c;
-
-        
-        
-      }
-
+// Function to perform subtraction
+float subtract() {
+    float a, b;
+    printf("Enter Number you want to subtract: \n");
+    a = scan();
+    printf("- \n");
+    b = scan();
+    float result = a - b;
+    printf("Answer: %g \n", result);
+    return result;
 }
 
-float divide(){
-    while(true){
-
-        float a, b, c;
-        printf("Enter Number you want to divide \n");
-        a = scan();
-        printf("/ \n");
-        b = scan();
-        c = a / b;
-        printf("Answer of ans is %g \n", c);
-        return c;
-
-        
-      }
-
-}
-
-float multiply(){
-    while(true){
-
-        float a, b, c;
-        printf("Enter Number you want to multiply \n");
-        a = scan();
-        printf("x \n");
-        b = scan();
-        c = a * b;
-        printf("Answer of ans is %g \n", c);
-        return c;
-
-      
+// Function to perform division
+float divide() {
+    float a, b;
+    printf("Enter Number you want to divide: \n");
+    a = scan();
+    printf("/ \n");
+    b = scan();
+    if (b == 0) {
+        printf("Error! Division by zero is not allowed.\n");
+        return 0;
     }
-
+    float result = a / b;
+    printf("Answer: %g \n", result);
+    return result;
 }
 
-float evenodd(){
-  while(true){
-    
-     int n;
-     printf("Enter an integer: ");
-     scanf("%d", &n);
-     (n % 2 == 0) ? printf("%d is even. \n", n) : printf("%d is odd. \n", n);
-     return n;
-  }
+// Function to perform multiplication
+float multiply() {
+    float a, b;
+    printf("Enter Number you want to multiply: \n");
+    a = scan();
+    printf("x \n");
+    b = scan();
+    float result = a * b;
+    printf("Answer: %g \n", result);
+    return result;
 }
 
-float factorial(){
-  while(true){
-    int n, i;
+// Function to check even or odd
+void check_even_odd() {
+    int n;
+    printf("Enter an integer: ");
+    scanf("%d", &n);
+    (n % 2 == 0) ? printf("%d is even. \n", n) : printf("%d is odd. \n", n);
+}
+
+// Function to calculate factorial
+void factorial() {
+    int n;
     unsigned long long fact = 1;
     printf("Enter an integer: ");
     scanf("%d", &n);
-    if (n < 0)
+    if (n < 0) {
         printf("Error! Factorial of a negative number doesn't exist. \n");
-    else {
-        for (i = 1; i <= n; ++i) {
+    } else {
+        for (int i = 1; i <= n; ++i) {
             fact *= i;
         }
         printf("Factorial of %d = %llu \n", n, fact);
     }
-
-    return fact;
-  }
 }
 
+// Function to display the menu and get the user's choice
+int show_menu() {
+    printf("Let's do some calculations\n");
+    printf("What do you want to do?\n");
+    printf("Here are your options:\n");
+    printf("0 - Exit\n");
+    printf("1 - Sum\n");
+    printf("2 - Difference\n");
+    printf("3 - Divide\n");
+    printf("4 - Multiply\n");
+    printf("5 - Check even or odd\n");
+    printf("6 - To calculate factorial\n");
+    printf("Enter any number listed above to continue: \n");
 
-int options(){
-    printf("lets do some calculations \n");
-    printf("what do you want to do \n");
-    printf("Here are your options \n");
-    printf("0- Exit \n");
-    printf("1- Sum \n");
-    printf("2- Difference \n");
-    printf("3- Divide \n");
-    printf("4- Multiply \n");
-    printf("5- Check even or odd \n");
-    printf("6- To calculate factorial \n");
-    printf("enter any number listed above to continue :) \n");
-
-    float value;
-    value = scan();
-    return value;
-
-
+    return (int)scan();
 }
 
-int main(){
-    int choice = options();
-    while(true){
+// Function to ask the user whether they want to continue, exit, or return to the menu
+char ask_to_continue() {
+    return scanc();
+}
 
-       if(choice == 0){
-         printf("Bye :)");
-         exit(0);
-       }
-       if(choice == 1){
-          while(true){
-             plus();
-             char d;
-             d = scanc();
-              if(d == 'y'){
-                continue;
-              }
-              if(d == 'n'){
-                printf("Bye :)");
-                exit(0);
-              }
-              if(d == 'm'){
-                main();
-              }
-              else{
-                err();
-                continue;
-              }
-            }
-            
-         
-        }
-       if(choice == 2){
-        while(true){
-             minus();
-             char d;
-             d = scanc();
-              if(d == 'y'){
-                continue;
-              }
-              if(d == 'n'){
-                printf("Bye :)");
-                exit(0);
-              }
-              if(d == 'm'){
-                main();
-              }
-              else{
-                err();
-                continue;
-              }
-            }
-        }
-        if(choice == 3){
-        while(true){
-             divide();
-             char d;
-             d = scanc();
-              if(d == 'y'){
-                continue;
-              }
-              if(d == 'n'){
-                printf("Bye :)");
-                exit(0);
-              }
-              if(d == 'm'){
-                main();
-              }
-              else{
-                err();
-                continue;
-              }
-            }
+int main() {
+    int choice;
 
-        }
-        if(choice == 4){
-        while(true){
-             multiply();
-             char d;
-             d = scanc();
-              if(d == 'y'){
-                continue;
-              }
-              if(d == 'n'){
-                printf("Bye :)");
+    while (1) {
+        choice = show_menu();
+        
+        switch(choice) {
+            case 0:
+                printf("Bye :)\n");
                 exit(0);
-              }
-              if(d == 'm'){
-                main();
-              }
-              else{
-                err();
-                continue;
-              }
-            }
 
+            case 1:
+                add();
+                break;
+
+            case 2:
+                subtract();
+                break;
+
+            case 3:
+                divide();
+                break;
+
+            case 4:
+                multiply();
+                break;
+
+            case 5:
+                check_even_odd();
+                break;
+
+            case 6:
+                factorial();
+                break;
+
+            default:
+                err();
+                break;
         }
-        if(choice == 5){
-        while(true){
-             evenodd();
-             char d;
-             d = scanc();
-              if(d == 'y'){
-                continue;
-              }
-              if(d == 'n'){
-                printf("Bye :)");
-                exit(0);
-              }
-              if(d == 'm'){
-                main();
-              }
-              else{
-                err();
-                continue;
-              }
-            }
 
-        }
-        if(choice == 6){
-        while(true){
-             factorial();
-             char d;
-             d = scanc();
-              if(d == 'y'){
-                continue;
-              }
-              if(d == 'n'){
-                printf("Bye :)");
-                exit(0);
-              }
-              if(d == 'm'){
-                main();
-              }
-              else{
-                err();
-                continue;
-              }
-            }
-
-        }
-        else{
-         err();
-         while(true){
-             char d;
-             printf("Retry ? \n");
-             printf("--- --- \n");
-             printf("y = yes \n");
-             printf("n = no \n");
-             printf("--- --- \n");
-             scanf(" %c", &d);
-             d = scanc();
-              if(d == 'y'){
-                main();
-              }
-              if(d == 'n'){
-                printf("Bye :)");
-                exit(0);
-              }
-              else{
-                err();
-                continue;
-              }
-            }
-
+        char continue_choice = ask_to_continue();
+        
+        if (continue_choice == 'n') {
+            printf("Bye :)\n");
+            exit(0);
+        } else if (continue_choice == 'm') {
+            continue; // Go back to the menu
+        } else if (continue_choice != 'y') {
+            err();
         }
     }
 
+    return 0;
 }
